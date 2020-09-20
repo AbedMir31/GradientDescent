7d4db3f69606fa07a0fdaa76e8d945fa57dda1db
AbedMir31 <abedmir31@gmail.com>
2020-09-20 16:07:36 -0500

    Part 1

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.axes
 from sklearn.model_selection import train_test_split
 
-LEARNING_RATE = .001
-EPOCH = 100000
+LEARNING_RATE = .01
+EPOCH = 10000
 
 
-def grad_descent(x, y, learning_rate, epoch):
-
-    m, c = .2, .2
-    n = float(len(x))
-    log_mse = []
-
-    for _ in range(epoch):
-        y_prime = y - (m*x + c)
-        m = m - learning_rate * (-2 * x.dot(y_prime).sum() / n)
-        c = c - learning_rate * (-2 * y_prime.sum() / n)
-        log_mse.append(mean_squared_error(y, (m * x + c)))
-
-    return m, c, log_mse
+def grad_descent(x, y, w, learning_rate, epoch):
+    parameterlog = []
+    for ep in range(0, epoch):
+        pred = x * w
+        error = pred - y
+        mse = calculatemse(len(x), pred, y)
+        gradient = x.T.dot(error) / x.shape[0]
+        w += -learning_rate * gradient
+        print('epoch: %d\tmse: %.2f' % (ep, mse))
+        parameterlog.append(mse)
+    return w, parameterlog
 
 
-def mean_squared_error(y_true, y_pred):
-    return np.square(np.subtract(y_true, y_pred)).mean()
+def calculatemse(n, h, y):
+    return (1/(2*n)) * (np.sum(h - y) ** 2)
 
 
 if __name__ == '__main__':
@@ -34,26 +30,18 @@ if __name__ == '__main__':
     totalX = data.iloc[0:1000, 10]  # X = Temperature Column
     totalY = data.iloc[0:1000, 16]  # Y = Biker Count
 
-    """
-    plt.scatter(X, Y)
-    plt.xlabel('Temperature (C)', fontsize=12)
-    plt.ylabel('Biker Count', fontsize=12)
-    plt.show()
-    """
-
     X_train, X_test = train_test_split(totalX, test_size=.2, random_state=69)
     Y_train, Y_test = train_test_split(totalY, test_size=.2, random_state=69)
 
-    m, b, mse_log = grad_descent(X_train, Y_train, LEARNING_RATE, EPOCH)
-    Y_pred = m*X_train + b
+    weights = np.random.uniform(low=0.0, high=1.0, size=(len(X_train)))
 
+    # TRAIN
+    new_weight, oldlog = grad_descent(X_train, Y_train, weights, LEARNING_RATE, EPOCH)
     log = open('log.txt', 'a')
-    log.write('learning rate: ' + str(LEARNING_RATE) + '\t')
-    log.write('epoch: ' + str(EPOCH) + '\t')
-    log.write('trial beginning MSE: ' + "{:.5f}".format(mse_log[0]) + '\t\t')
-    log.write('trial completion MSE: ' + "{:.5f}".format(mse_log[-1]) + '\n')
+    log.write('training data:\tstarting mse: %.2f\tending mse: %.2f\n' % (oldlog[0], oldlog[-1]))
 
-    #  y_pred = grad_descent(X_train, Y_train, LEARNING_RATE, NUM_ITERATIONS, "train")
-    #  plt.scatter(X_test, Y_test)
-    #  plt.plot([min(X_test), max(X_test)], [min(y_pred), max(y_pred)], color='red')
-    #  plt.show()
+    # TEST
+    new_weight, newlog = grad_descent(X_train, Y_train, new_weight, LEARNING_RATE, EPOCH)
+    log = open('log.txt', 'a')
+    log.write('test data:\t starting mse: %f\tending mse: %.2f\n' % (newlog[0], newlog[-1]))
+
